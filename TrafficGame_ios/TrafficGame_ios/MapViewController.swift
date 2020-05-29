1701dcbee1e0532a609fc8ab3de12f5f1a1fd6d0
周福 <602933854@qq.com>
2020-05-29 20:49:14 +0800

    给地图上的点，添加序号信息

--- TrafficGame_ios/TrafficGame_ios/MapViewController.swift
+++ TrafficGame_ios/TrafficGame_ios/MapViewController.swift
@@ -20,6 +20,7 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         // 1.在ViewController实现MKMapViewDelegate协议的委托
         mapView.delegate = self
+        
         // 获取用户当前的位置：
         AF.request("http://127.0.0.1:5000/getResult",method: .get).response { response in
             //debugPrint(response)
@@ -57,12 +58,24 @@ class MapViewController: UIViewController {
             
             self.matlabResult = MatlabResult(longCost: long!, carPath: pathtList, airplanPath: pathdList, save_ratefor0: save_ratefor0!, save_ratefor1: save_ratefor1!)
                      
-            
+            // 创建一个 points of pain 结果的点集合：
             var locationCoordinate2Ds = [CLLocationCoordinate2D]()
+            
+            // 创建一个 注释的集合：
+            var pointAnnotations = [MKAnnotation]()
+            
             
             if let matlabResult = self.matlabResult{
                     for index in matlabResult.carPath{
                         locationCoordinate2Ds.append(CLLocationCoordinate2D(latitude: index.latitude, longitude: index.longitude))
+                        
+                        let pointAnnotation = MKPointAnnotation()
+                    
+                        pointAnnotation.title = "\(index.No)"
+                        //print(pointAnnotation.title!)
+                        
+                        pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: index.latitude, longitude: index.longitude)
+                        pointAnnotations.append(pointAnnotation)
                     }
 
             }
@@ -79,16 +92,20 @@ class MapViewController: UIViewController {
 //                    let points: [CLLocationCoordinate2D]
 //                    points = [point1, point2, point3, point4, point5]
                      // let geodesic = MKPolyline(coordinates: points, count: 5)
+            
             let geodesic = MKGeodesicPolyline(coordinates: locationCoordinate2Ds, count: locationCoordinate2Ds.count)
             self.mapView.addOverlay(geodesic)
             
+            // 动画setRegion
                     UIView.animate(withDuration: 1.5, animations: { () -> Void in
                         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
                         
                         let region1 = MKCoordinateRegion(center: locationCoordinate2Ds[0], span: span)
                         self.mapView.setRegion(region1, animated: true)
                     })
-           
+            
+                   // 6.注释显示在地图上
+                   self.mapView.showAnnotations(pointAnnotations, animated: true )
 
         }
         
@@ -111,7 +128,7 @@ class MapViewController: UIViewController {
 //        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
 //        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
 //
-//        // 5. 添加注释，显示地标的名称
+//        // 5. 注释对象： 添加注释，显示地标的名称
 //        let sourceAnnotation = MKPointAnnotation()
 //        sourceAnnotation.title = "Times Square"
 //
@@ -130,7 +147,9 @@ class MapViewController: UIViewController {
 //
 //        // 6.注释显示在地图上
 //        self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
-//
+
+        
+        
 //        // 7. MKDirectionsRequest类用于计算路线。
 //        let directionRequest = MKDirections.Request()
 //        directionRequest.source = sourceMapItem
@@ -171,9 +190,9 @@ class MapViewController: UIViewController {
 //        let routeLine = MKPolyline(coordinates: [sourceLocation,destinationLocation], count: 2)
 //        routeLine.title = "two"
 //        self.mapView.addOverlay(routeLine)
-//
+//        指定 地图的中心部位：
 //        let rect1 = routeLine.boundingMapRect
-//
+
 //        self.mapView.setRegion(MKCoordinateRegion(rect1), animated: true)
         
         
@@ -194,6 +213,7 @@ class MapViewController: UIViewController {
 }
 extension MapViewController:MKMapViewDelegate{
     
+    // 画线的函数：
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         
 //        let renderer = MKPolylineRenderer(overlay: overlay)
@@ -219,18 +239,20 @@ extension MapViewController:MKMapViewDelegate{
             return routeLineView
     }
     
-    // 自定义标记的样式：
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            return nil
-        }
-        
-        else {
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
-            annotationView.image = UIImage(systemName: "hare.fill")
-//            annotationView.annotation.
-            return annotationView
-        }
-    }
+    
+    // 自定义标记的样式： 系统的点是一个大头针：
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        if annotation is MKUserLocation {
+//            return nil
+//        }else {
+//            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+//            annotationView.canShowCallout = true
+//
+//            //annotationView.image = UIImage(systemName: "hare.fill")
+//            return annotationView
+//        }
+//    }
+    
+    
     
 }
